00f46e043ff578c64866a86444fcabfb398efc25
Vitalii Matvieiev <vitalik.matveev133@gmail.com>
2024-04-24 21:14:13 -0500

    init

---
+++ QueueRsolver/QueueRsolver/Program.cs
@@ -0,0 +1,8 @@
+﻿using QueueRsolver;
+
+(string, int) redisconectionAOF = ("localhost:6380", 1000000);
+(string, int) redisconectionRDB = ("localhost:6381", 1000000);
+(string, int) beanstalkdConnection = ("localhost:11300", 1000000);
+
+Resolver.BeanstalkTest(beanstalkdConnection.Item1, beanstalkdConnection.Item2).Wait();
+//Resolver.RedisTest(redisconectionRDB.Item1, redisconectionRDB.Item2).Wait();
